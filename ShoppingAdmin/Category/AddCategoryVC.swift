714421175455fa21b2a72e9121fa49b306e42952
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-05-28 01:23:00 +0300

    AddCategoryVC image picker and tap gesture for select image

--- ShoppingAdmin/Category/AddCategoryVC.swift
+++ ShoppingAdmin/Category/AddCategoryVC.swift
@@ -38,6 +38,7 @@ class AddCategoryVC: UIViewController {
         iv.layer.cornerRadius = 12
         iv.layer.borderWidth = 0.5
         iv.layer.borderColor = UIColor.darkGray.cgColor
+        iv.clipsToBounds = true
        
         return iv
     }()
@@ -67,36 +68,74 @@ class AddCategoryVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         view.addSubview(stackView)
         view.addSubview(categoryimgView)
         view.addSubview(addBtn)
 
-       
-        stackView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
-                         leading: view.leadingAnchor,
-                         bottom: nil,
-                         trailing: view.trailingAnchor,
-                         padding: .init(top: 50, left: 10, bottom: 0, right: 10),
-                         size: .init(width: 0, height: 78))
-        
-        categoryimgView.anchor(top:stackView.bottomAnchor ,
-                               leading: nil,
-                               bottom: nil,
-                               trailing: nil,
-                               padding: .init(top: 20, left: 0, bottom: 0, right: 0),
-                               size: .init(width: 260, height: 235))
-        categoryimgView.centerXInSuperview()
-        
-        addBtn.anchor(top: categoryimgView.bottomAnchor,
-                      leading: view.leadingAnchor,
-                             bottom: nil,
-                      trailing: view.trailingAnchor,
-                             padding: .init(top: 50, left: 10, bottom: 0, right: 10),
-                             size: .init(width: 0, height: 60))
-  
-                         
-        view.backgroundColor = .white
+        setConstraints()
+        addTapGestureForImage()
     }
     
+    private func addTapGestureForImage(){
+        let tap = UITapGestureRecognizer(target: self, action: #selector(clickImg(_:)))
+        tap.numberOfTapsRequired = 1
+        categoryimgView.isUserInteractionEnabled = true
+        categoryimgView.addGestureRecognizer(tap)
+    }
+    
+    @objc func clickImg( _ tap: UITapGestureRecognizer){
+        launchImagePicker()
+    }
+}
 
+//MARK: -
+extension AddCategoryVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func launchImagePicker() {
+        let imagePicker  = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.originalImage] as? UIImage else { return }
+        
+        categoryimgView.contentMode = .scaleAspectFill
+        categoryimgView.image = image
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
+//MARK: - Constraints
+extension AddCategoryVC {
+    private func setConstraints(){
+        
+         stackView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                          leading: view.leadingAnchor,
+                          bottom: nil,
+                          trailing: view.trailingAnchor,
+                          padding: .init(top: 50, left: 10, bottom: 0, right: 10),
+                          size: .init(width: 0, height: 78))
+         
+         categoryimgView.anchor(top:stackView.bottomAnchor ,
+                                leading: nil,
+                                bottom: nil,
+                                trailing: nil,
+                                padding: .init(top: 20, left: 0, bottom: 0, right: 0),
+                                size: .init(width: 260, height: 235))
+         categoryimgView.centerXInSuperview()
+         
+         addBtn.anchor(top: categoryimgView.bottomAnchor,
+                       leading: view.leadingAnchor,
+                              bottom: nil,
+                       trailing: view.trailingAnchor,
+                              padding: .init(top: 50, left: 10, bottom: 0, right: 10),
+                              size: .init(width: 0, height: 60))
+    }
 }

28c9ecd5f628911d277c5b2d21e1df76104d7530
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-05-27 16:11:42 +0300

    CartVC and CartCollectionCell created

---
+++ ShoppingApp/Cells/CartCell/CartCollectionCell.swift
@@ -0,0 +1,14 @@
+//
+//  CartTableCell.swift
+//  ShoppingApp
+//
+//  Created by ugur-pc on 27.05.2022.
+//
+
+import UIKit
+
+class CartCollectionCell: UICollectionViewCell {
+    
+    static var identifier = "CartCollectionCell"
+    
+}
--- ShoppingApp/Controllers/Cart/CartVC.swift
+++ ShoppingApp/Controllers/Cart/CartVC.swift
@@ -7,12 +7,105 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class CartVC: UIViewController {
+    
+    // General CollectionView
+      private let generalCollectionView: UICollectionView = {
+          let layout = UICollectionViewFlowLayout()
+          layout.scrollDirection = .vertical
+          let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+          cv.showsHorizontalScrollIndicator = false
+          cv.backgroundColor = .systemBackground
+         
+          
+          //register cells
+          cv.register(CartCollectionCell.self,
+                      forCellWithReuseIdentifier: CartCollectionCell.identifier)
+          return cv
+      }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+        setupViews()
+        setConstraints()
+        generalCollectionView.delegate = self
+        generalCollectionView.dataSource = self
+        
+    }
+    
+    private func setupViews(){
+        view.addSubview(generalCollectionView)
+    }
+    
+}
+extension CartVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    // numberOfItemsInSection ( how many cells )
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    
+    
+    // cellForItemAt
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = generalCollectionView.dequeueReusableCell(withReuseIdentifier: CartCollectionCell.identifier, for: indexPath) as! CartCollectionCell
+           
+        cell.backgroundColor = .orange
+        cell.layer.cornerRadius = 15
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+       
+            return 15
+        
+    }
+    
+    
+}
+
+//MARK: - DelegateFlowLayout
+extension CartVC: UICollectionViewDelegateFlowLayout {
+    
+    
+    // sizeForItemAt, cell -> w,h
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+    
+        return CGSize(width: generalCollectionView.frame.width - 35,
+                      height: generalCollectionView.frame.width - 300)
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+       
+        return UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)
     }
 }
+
+
+
+//MARK: -
+extension CartVC {
+    private func setConstraints(){
+        generalCollectionView.fillSuperview()
+    }
+}
+
+

c0516305080933ac5a1620e148d01bda919a7969
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-06-04 09:25:59 +0300

    ProfileVC edited

---
+++ ShoppingApp/Cells/ProfileCells/BottomViews/FavItemsViews.swift
@@ -0,0 +1,74 @@
+//
+//  FavItemsViews.swift
+//  ShoppingApp
+//
+//  Created by ugur-pc on 4.06.2022.
+//
+
+import UIKit
+
+class FavItemsViews: UIView {
+    
+    private let favImgView: UIImageView = {
+         let iv = UIImageView()
+         iv.image = UIImage(named: "favimg")
+         iv.contentMode = .scaleToFill
+         iv.clipsToBounds = true
+         iv.layer.cornerRadius = 15
+         iv.layer.borderWidth = 0.3
+         return iv
+    }()
+    
+    private let favLbl: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 27, weight: .bold)
+        label.text = "4544"
+        label.layer.shadowColor = UIColor.black.cgColor
+        label.layer.shadowOpacity = 2.5
+        label.layer.shadowRadius = 2.0
+      
+        label.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+        label.textColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("not imp")
+    }
+    
+    private func setupViews(){
+        addSubview(favImgView)
+        addSubview(favLbl)
+    }
+
+}
+
+//MARK: -
+extension FavItemsViews {
+    private func  setConstraints() {
+        favImgView.anchor(top: topAnchor,
+                          leading: leadingAnchor,
+                          bottom: bottomAnchor,
+                          trailing: trailingAnchor,
+                          padding: .init(top: 0, left: 0, bottom: 0, right: 0),
+                          size: .init(width: 0, height: 100)
+        )
+        
+        favLbl.anchor(top: nil,
+                      leading: nil,
+                      bottom: bottomAnchor,
+                      trailing: trailingAnchor,
+                      padding: .init(top: 15, left: 4, bottom: 0, right: 0),
+                      size: .init(width: 80, height: 40))
+        
+    }
+}
--- ShoppingApp/Cells/ProfileCells/ProfileBottomCell.swift
+++ ShoppingApp/Cells/ProfileCells/ProfileBottomCell.swift
@@ -9,4 +9,57 @@ import UIKit
 
 class ProfileBottomCell: UICollectionViewCell {
     static var identifier = "ProfileBottomCell"
+    
+    private let allFavItemsView = FavItemsViews()
+    private let allCartItemsView = CartItemsView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("not imp")
+    }
+}
+
+//MARK: -
+extension ProfileBottomCell {
+    private func setupViews(){
+        addSubview(allFavItemsView)
+        addSubview(allCartItemsView)
+        
+        allFavItemsView.layer.shadowColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1).cgColor
+        allFavItemsView.layer.shadowOffset = CGSize(width: 2, height: 2)
+        allFavItemsView.layer.shadowOpacity = 2.8
+        allFavItemsView.layer.shadowRadius = 8
+        
+        
+        allCartItemsView.layer.shadowColor = #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1).cgColor
+        allCartItemsView.layer.shadowOffset = CGSize(width: 2, height: 2)
+        allCartItemsView.layer.shadowOpacity = 2.8
+        allCartItemsView.layer.shadowRadius = 8
+        
+        allFavItemsView.layer.cornerRadius = 15
+        allCartItemsView.layer.cornerRadius = 15
+    }
+    
+    private func setConstraints(){
+        allFavItemsView.anchor(top: contentView.topAnchor,
+                               leading: contentView.leadingAnchor,
+                               bottom: nil,
+                               trailing: nil,
+                               padding: .init(top: 20, left: 4, bottom: 0, right: 4),
+                               size: .init(width: contentView.frame.width / 2 - 10,
+                                           height: 200))
+        
+        allCartItemsView.anchor(top: contentView.topAnchor,
+                                leading: allFavItemsView.trailingAnchor,
+                                bottom: nil,
+                                trailing: contentView.trailingAnchor,
+                                padding: .init(top: 20, left: 7, bottom: 0, right: 4),
+                                size: .init(width: contentView.frame.width / 2 - 11,
+                                            height: 200))
+    }
 }
